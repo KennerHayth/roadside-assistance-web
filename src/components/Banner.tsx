3945cbe47cbb080c9eb626972cc00230f7d66349
Kenner Hayth <kennerspc@gmail.com>
2026-03-12 18:53:46 -0400

    added image to the banner and changed the button layout

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,6 @@
 
 import Button from "./Button"
 
-
 export default function Banner(){
     return(
     <div style={{
@@ -10,21 +9,25 @@ export default function Banner(){
         left: "0",
         width: "100%",
         zIndex: 1000,
-        backgroundImage: "linear-gradient(to bottom, #1b1815, #a85823 80%)",
+        backgroundImage: "linear-gradient(to bottom, #1b1815, #71797E 80%)",
         borderBottom: "solid",
         borderBottomColor: "black",
         padding: "25px",
         paddingRight: "30px",
         fontFamily: "'Bebas Neue', sans-serif",
-        textAlign: "center",
+        justifyContent: "space-between",
+        alignItems:"center",
+        display:"flex",
+        
     }}> 
-
-    <Button label="Home" onClick={()=>{}} variant="primary"/>
-    <Button label="About us" onClick={()=>{}} variant="primary"/>
-    <Button label="24/7 Emergency Roadside" onClick={()=>{}} variant="primary"/>
-    <Button label="Contact" onClick={()=>{}} variant="primary"/>
-    <Button label="Pricing" onClick={()=>{}} variant="primary"/>
-
+    <img src="/LOGO_transparent.PNG" alt= "Highway Heros Logo" style={{width: "150px", height:"auto"}}/>
+        <div style={{display:"flex", gap:"10px", paddingRight:"425px"}}>
+        <Button label="Home" onClick={()=>{}} variant="primary"/>
+        <Button label="About us" onClick={()=>{}} variant="primary"/>
+        <Button label="24/7 Emergency Roadside" onClick={()=>{}} variant="primary"/>
+        <Button label="Contact" onClick={()=>{}} variant="primary"/>
+        <Button label="Pricing" onClick={()=>{}} variant="primary"/>
+        </div>
 
     </div>
     )}
